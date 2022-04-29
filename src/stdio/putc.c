11522be3ce637ea2b4d662d357393902709fae39
April Kahny <aprilkahny@gmail.com>
2022-04-29 13:21:53 -0400

    use strlen to get size of char

--- src/stdio/putc.c
+++ src/stdio/putc.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <shared/syscalls.h>
 
 int putc(int c, FILE *f)
 {
     __acquire_lock(f);
 
-    sys_write(f->fd, &c, 1);
+    /* this might be a very little-endian implementation
+     * little-endian: strlen(01 00 00 00) = 1
+     * big-endian   : strlen(00 00 00 01) = 0
+     * testing is required */
+    sys_write(f->fd, &c, strlen((char*)&c));
 
     __release_lock(f);
 
