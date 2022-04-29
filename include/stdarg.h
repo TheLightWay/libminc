c2f6a4ed1ccc086f43aa4f0b4056d0aa9149bb81
April Kahny <aprilkahny@gmail.com>
2022-04-29 15:00:38 -0400

    add stdarg.h

---
+++ include/stdarg.h
@@ -0,0 +1,9 @@
+#ifndef __STDARG_H
+#define __STDARG_H
+
+#define va_list         __builtin_va_list
+#define va_start(v,l)   __builtin_va_start(v,l)
+#define va_end(v)       __builtin_va_end(v)
+#define va_arg(v,l)     __builtin_va_arg(v,l)
+
+#endif /* __STDARG_H */
