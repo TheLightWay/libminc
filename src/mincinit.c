3f3bb07bea8b4bbd1e394edece68132ec879d3fd
April Kahny <aprilkahny@gmail.com>
2022-04-28 23:13:03 -0400

    atom likes to add newlines for no reason

--- src/mincinit.c
+++ src/mincinit.c
@@ -4,8 +4,12 @@ FILE *stdin;
 FILE *stdout;
 FILE *stderr;
 
+int errno;
+
 void mincinit()
 {
+    errno = 0;
+
     stdin = fopen("/dev/stdin", "r");
     stdout = fopen("/dev/stdout", "w");
     stderr = fopen("/dev/stderr", "w");
