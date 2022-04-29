d7f271ea93a8c13d7d4fdb02aa82feb88cbd8a28
April Kahny <aprilkahny@gmail.com>
2022-04-29 12:48:34 -0400

    add all macros for sys_open

--- platform/shared/syscalls.h
+++ platform/shared/syscalls.h
@@ -9,10 +9,38 @@ ssize_t sys_read(int fd, void *buf, size_t count);
 /* write to object with file descriptor */
 ssize_t sys_write(int fd, const void *buf, size_t count);
 
-#define O_RDONLY    00
-#define O_WRONLY    01
-#define O_RDWR      02
-#define O_CREAT     0100
+#define O_RDONLY    0x0
+#define O_WRONLY    0x1
+#define O_RDWR      0x2
+#define O_APPEND    0x400
+#define O_ASYNC     0x2000
+#define O_CLOEXEC   0x80000
+#define O_CREAT     0x40
+#define O_DIRECTORY 0x10000
+#define O_DSYNC     0x1000
+#define O_EXCL      0x80
+#define O_NOCTTY    0x100
+#define O_NOFOLLOW  0x20000
+#define O_NONBLOCK  0x800
+#define O_NDELAY    0x800
+#define O_SYNC      0x101000
+#define O_TRUNC     0x200
+
+#define S_IRWXU     0x1c0
+#define S_IRUSR     0x100
+#define S_IWUSR     0x80
+#define S_IXUSR     0x40
+#define S_IRGRP     0x20
+#define S_IWGRP     0x10
+#define S_IXGRP     0x8
+#define S_IRWXO     0x7
+#define S_IROTH     0x4
+#define S_IWOTH     0x2
+#define S_IXOTH     0x1
+#define S_ISUID     0x800
+#define S_ISGID     0x400
+#define S_ISVTX     0x200
+
 /* open file */
 int sys_open(const char *pathname, int flags, ...);
 
@@ -26,6 +54,7 @@ int sys_close(unsigned int fd);
 #define MAP_ANONYMOUS   0x20
 #define MAP_SHARED      0x01
 #define MAP_PRIVATE     0x02
+
 /* allocate memory to the heap */
 void *sys_mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
 
