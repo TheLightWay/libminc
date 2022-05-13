f4aa40f9e1676975f5b85ce941fc259fa63735a6
April Kahny <aprilkahny@gmail.com>
2022-05-13 01:56:17 -0400

    replace strlen call with a constant size

--- src/stdio/putc.c
+++ src/stdio/putc.c
@@ -6,11 +6,7 @@ int putc(int c, FILE *f)
 {
     __acquire_lock(f);
 
-    /* this might be a very little-endian implementation
-     * little-endian: strlen(01 00 00 00) = 1
-     * big-endian   : strlen(00 00 00 01) = 0
-     * testing is required */
-    sys_write(f->fd, &c, strlen((char*)&c));
+    sys_write(f->fd, &c, sizeof(c));
 
     __release_lock(f);
 
