49d437addcbe47bea088de3a4c3fbc58fbcdeaac
April Kahny <aprilkahny@gmail.com>
2022-05-13 01:55:45 -0400

    add fpos_t, EOF, macro standard streams

--- include/stdio.h
+++ include/stdio.h
@@ -4,14 +4,20 @@
 /* types */
 #include "types/size_t.h"
 #include "types/FILE.h"
+#include "types/fpos_t.h"
 
 /* constants */
 #include "constants/NULL.h"
+#include "constants/EOF.h"
 
 /* globals */
 extern FILE *stdin;
 extern FILE *stdout;
 extern FILE *stderr;
+
+#define stdin stdin
+#define stdout stdout
+#define stderr stderr
 
 /* print str to stdout */
 int puts(const char *str);
