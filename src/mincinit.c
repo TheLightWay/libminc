f7b5e44cc6f81893d598ea1a3d1d5e1a8bd2a3fd
April Kahny <aprilkahny@gmail.com>
2022-08-02 00:36:41 -0400

    do not reopen stdin, out, err

--- src/mincinit.c
+++ src/mincinit.c
@@ -6,11 +6,15 @@ FILE *stderr;
 
 __thread int errno;
 
+FILE __stdin = { 0, 0 };
+FILE __stdout = { 1, 0 };
+FILE __stderr = { 2, 0 };
+
 void mincinit()
 {
     errno = 0;
 
-    stdin = fopen("/dev/stdin", "r");
-    stdout = fopen("/dev/stdout", "w");
-    stderr = fopen("/dev/stderr", "w");
+    stdin = &__stdin;
+    stdout = &__stdout;
+    stderr = &__stderr;
 }
